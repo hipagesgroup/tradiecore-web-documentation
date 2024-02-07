85ac8cf0e641b6f61cbee06e5a31e079771a052d
Mela-C <melanieching@hipagesgroup.com.au>
2024-02-07 11:21:57 +1100

    Committing file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk7.js file:///C:/Review - Tradiecore/Data/SearchTopic_Chunk1.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk6.js file:///C:/Review - Tradiecore/Content/Jobs/Copy of Add_Notes.htm file:///C:/Review - Tradiecore/Content/Jobs/Add_Notes.htm file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk5.js file:///C:/Review - Tradiecore/Data/SearchStem_Chunk1.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk4.js file:///C:/Review - Tradiecore/Content/Invoices/Create_Invoice.htm file:///C:/Review - Tradiecore/Content/Customers/Add_Customer.htm file:///C:/Review - Tradiecore/Content/A_Introduction Topics/Calendar_Intro.htm file:///C:/Review - Tradiecore/Content/Jobs/Add_Job.htm file:///C:/Review - Tradiecore/Content/Jobs/Add_Appt.htm file:///C:/Review - Tradiecore/Skins/Default/Stylesheets/Components/Tablet.css file:///C:/Review - Tradiecore/Skins/Default/Stylesheets/Components/Styles.css file:///C:/Review - Tradiecore/Skins/Default/Stylesheets/Components/Print.css file:///C:/Review - Tradiecore/Skins/Default/Stylesheets/Components/Mobile.css file:///C:/Review - Tradiecore/Resources/Scripts/require.config.js file:///C:/Review - Tradiecore/Data/Tocs/Master_Chunk0.js file:///C:/Review - Tradiecore/Data/Tocs/Master.js file:///C:/Review - Tradiecore/Data/SearchUrl_Chunk0.js file:///C:/Review - Tradiecore/Data/SearchTopic_Chunk0.js file:///C:/Review - Tradiecore/Data/SearchStem_Chunk0.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk3.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk2.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk1.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk0.js file:///C:/Review - Tradiecore/Data/Search.js file:///C:/Review - Tradiecore/Data/HelpSystem.xml file:///C:/Review - Tradiecore/Data/HelpSystem.js file:///C:/Review - Tradiecore/Content/Resources/Stylesheets/MainStyles.css file:///C:/Review - Tradiecore/Content/Quotes/Quotes_Intro.htm file:///C:/Review - Tradiecore/Content/Quotes/Add_Quote.htm file:///C:/Review - Tradiecore/Content/Invoices/Invoices_Intro.htm file:///C:/Review - Tradiecore/Default.mcwebhelp file:///C:/Review - Tradiecore/Content/Quotes/Create_Item.htm file:///C:/Review - Tradiecore/Content/Resources/Images/Intro&Common Images/Add_Icon.png file:///C:/Review - Tradiecore/Content/Resources/Images/Intro&Common Images/Add_Icon_27x17.png file:///C:/Review - Tradiecore/Content/Resources/Images/Intro&Common Images/Add_Icon_30x19.png file:///C:/Review - Tradiecore/Content/Resources/Images/Quotes/Items_Section.png file:///C:/Review - Tradiecore/Content/Resources/Images/Quotes/Items_Section_thumb_0_170.png file:///C:/Review - Tradiecore/Content/Resources/Images/Quotes/Item_New.png file:///C:/Review - Tradiecore/Content/Resources/Images/Quotes/Item_New_TaxRate.png file:///C:/Review - Tradiecore/Content/Resources/Images/Quotes/Item_New_TaxRate_thumb_0_170.png file:///C:/Review - Tradiecore/Content/Resources/Images/Quotes/Item_New_thumb_0_170.png file:///C:/Review - Tradiecore/Content/Resources/Images/Quotes/QuotesList_New.png file:///C:/Review - Tradiecore/Content/Resources/Images/Quotes/QuotesList_New_thumb_0_170.png file:///C:/Review - Tradiecore/Content/Resources/Images/Settings/Items_Screen_Main.png file:///C:/Review - Tradiecore/Content/Resources/Images/Settings/Items_Screen_Main_238x513.png file:///C:/Review - Tradiecore/Content/Resources/Images/Settings/Items_Screen_Main_thumb_0_170.png file:///C:/Review - Tradiecore/Content/Resources/Images/Settings/Item_Screen.png file:///C:/Review - Tradiecore/Content/Resources/Images/Settings/Item_Screen_thumb_0_170.png file:///C:/Review - Tradiecore/Content/Settings/Line Items/Items_Intro.htm file:///C:/Review - Tradiecore/Content/Settings/Line Items/LineItems_Manage.htm file:///C:/Review - Tradiecore/HTML5 - Top Navigation.4.log.zip

--- Data/Search.js
+++ Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0,74],m:[],u:['../Content/A_Introduction Topics/Basic Steps.htm'],s:['$20','preview'],p:[['$20',2],['billing',13],['dollar',2],['information',41],['of',34],['screen',55],['theophrastus',69],['yes',2]]});
+define({r:0.5,st:'Stem',n:1,t:[0,74],m:[],u:['../Content/A_Introduction Topics/Basic Steps.htm'],s:['$20','potenti'],p:[['$20',2],['being',21],['do',41],['increases',37],['notifications',49],['same',2],['the',47],['will',50]]});
--- Resources/Scripts/require.config.js
+++ Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638424832402541132'
+    urlArgs: 't=638429016595388871'
 });
