fe7b3395f8ca1d2214740e7270ff14e3d4abc06b
Mela-C <melanieching@hipagesgroup.com.au>
2024-06-21 13:12:47 +1000

    Committing file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk10.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk9.js file:///C:/Review - Tradiecore/Content/Settings/My Business/Payment_Terms.htm file:///C:/Review - Tradiecore/Content/Settings/Payments/Terms_Conditions.htm file:///C:/Review - Tradiecore/Content/Settings/Payments/Payments_Intro.htm file:///C:/Review - Tradiecore/Content/Settings/Payments/New_Stripe_Business.htm file:///C:/Review - Tradiecore/Content/Settings/Payments/hipagespayments_Connecting.htm file:///C:/Review - Tradiecore/Content/Settings/Payments/Bank_Transfer.htm file:///C:/Review - Tradiecore/Content/Settings/My Business/Profile_Photos.htm file:///C:/Review - Tradiecore/Content/Settings/My Business/Profile_Image.htm file:///C:/Review - Tradiecore/Content/Settings/My Business/Business_Headline_Description.htm file:///C:/Review - Tradiecore/Content/Settings/My Business/BP_Sync_Google_Reviews.htm file:///C:/Review - Tradiecore/Content/Settings/My Business/Business_Details.htm file:///C:/Review - Tradiecore/Sitemap.xml file:///C:/Review - Tradiecore/Content/Settings/Settings&Support.htm file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk8.js file:///C:/Review - Tradiecore/Content/Settings/Help & Support/Privacy_Policy.htm file:///C:/Review - Tradiecore/Content/Settings/Help & Support/Message_Support.htm file:///C:/Review - Tradiecore/Content/Settings/Help & Support/Leads_Pricing.htm file:///C:/Review - Tradiecore/Content/Settings/Help & Support/HelpSupport_Intro.htm file:///C:/Review - Tradiecore/Content/Settings/Help & Support/Email_Support.htm file:///C:/Review - Tradiecore/Content/Settings/Help & Support/Call_Support.htm file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk7.js file:///C:/Review - Tradiecore/Content/Settings/Notifications/Notifications_Info.htm file:///C:/Review - Tradiecore/Content/Settings/Notifications/Enable_Disable_Notifications.htm file:///C:/Review - Tradiecore/Data/SearchTopic_Chunk1.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk6.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk5.js file:///C:/Review - Tradiecore/Content/Settings/Account_Settings.htm file:///C:/Review - Tradiecore/Data/SearchStem_Chunk1.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk4.js file:///C:/Review - Tradiecore/Resources/Scripts/require.config.js file:///C:/Review - Tradiecore/Data/Tocs/Master_Chunk0.js file:///C:/Review - Tradiecore/Data/Tocs/Master.js file:///C:/Review - Tradiecore/Data/SearchUrl_Chunk0.js file:///C:/Review - Tradiecore/Data/SearchStem_Chunk0.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk3.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk2.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk1.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk0.js file:///C:/Review - Tradiecore/Data/Search.js file:///C:/Review - Tradiecore/Data/HelpSystem.xml file:///C:/Review - Tradiecore/Data/HelpSystem.js file:///C:/Review - Tradiecore/Content/Integrations/Xero_Disconnect.htm file:///C:/Review - Tradiecore/Content/Integrations/Xero_Connecting.htm file:///C:/Review - Tradiecore/Content/Integrations/Quickbooks_Disconnecting.htm file:///C:/Review - Tradiecore/Content/Integrations/Quickbooks_Connecting.htm file:///C:/Review - Tradiecore/Content/Integrations/MYOB_Disconnecting.htm file:///C:/Review - Tradiecore/Content/Integrations/MYOB_Connection.htm file:///C:/Review - Tradiecore/Content/Integrations/hipages_Connecting.htm file:///C:/Review - Tradiecore/Content/Integrations/Chart_Accounts.htm file:///C:/Review - Tradiecore/Content/Integrations/AccountIntegrations_Intro.htm file:///C:/Review - Tradiecore/Default.mcwebhelp file:///C:/Review - Tradiecore/Content/Resources/Images/Settings/Settings & Support/Settings&Support_2.3.1.png file:///C:/Review - Tradiecore/Content/Resources/Images/Settings/Settings & Support/Settings&Support_2.3.1_238x513.png file:///C:/Review - Tradiecore/HTML5 - Top Navigation.4.log.zip

--- Data/Search.js
+++ Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0,71],m:[],u:['../Content/A_Introduction Topics/Basic Steps.htm'],s:['$20','map'],p:[['$20',15],['are',20],['contain',29],['euismod',116],['in',71],['me',55],['paid',98],['screens',2],['the',7],['turn',75],['your',23]]});
+define({r:0.5,st:'Stem',n:1,t:[0,71],m:[],u:['../Content/A_Introduction Topics/Basic Steps.htm'],s:['$20','map'],p:[['$20',15],['are',18],['contain',27],['euismod',94],['in',69],['me',15],['paid',95],['screen',125],['the',7],['turn',75],['your',23]]});
--- Resources/Scripts/require.config.js
+++ Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638545712623215034'
+    urlArgs: 't=638545722736153151'
 });
