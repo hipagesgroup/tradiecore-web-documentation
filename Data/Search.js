2db94dbebf039f4feec5cd6c8ec24da31e015abc
Mela-C <melanieching@hipagesgroup.com.au>
2024-10-14 17:27:52 +1100

    Committing file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk10.js file:///C:/Review - Tradiecore/Content/Invoices/Create_Deposit_Invoice.htm file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk9.js file:///C:/Review - Tradiecore/Sitemap.xml file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk8.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk7.js file:///C:/Review - Tradiecore/Data/SearchTopic_Chunk1.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk6.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk5.js file:///C:/Review - Tradiecore/Data/SearchStem_Chunk1.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk4.js file:///C:/Review - Tradiecore/Resources/Scripts/require.config.js file:///C:/Review - Tradiecore/Data/SearchUrl_Chunk0.js file:///C:/Review - Tradiecore/Data/SearchTopic_Chunk0.js file:///C:/Review - Tradiecore/Data/SearchStem_Chunk0.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk3.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk2.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk1.js file:///C:/Review - Tradiecore/Data/SearchPhrase_Chunk0.js file:///C:/Review - Tradiecore/Data/Search.js file:///C:/Review - Tradiecore/Data/HelpSystem.xml file:///C:/Review - Tradiecore/Data/HelpSystem.js file:///C:/Review - Tradiecore/Default.mcwebhelp file:///C:/Review - Tradiecore/Content/Resources/Videos/Deposit Invoices/Deposit Invoices.html file:///C:/Review - Tradiecore/Content/Resources/Videos/Deposit Invoices/Deposit Invoices.mp4 file:///C:/Review - Tradiecore/Content/Resources/Videos/Deposit Invoices/Deposit Invoices_config.xml file:///C:/Review - Tradiecore/Content/Resources/Videos/Deposit Invoices/Deposit Invoices_embed.css file:///C:/Review - Tradiecore/Content/Resources/Videos/Deposit Invoices/Deposit Invoices_player.html file:///C:/Review - Tradiecore/Content/Resources/Videos/Deposit Invoices/Deposit_Invoices_First_Frame.png file:///C:/Review - Tradiecore/Content/Resources/Videos/Deposit Invoices/Deposit_Invoices_Thumbnails.png file:///C:/Review - Tradiecore/Content/Resources/Videos/Deposit Invoices/scripts/config_xml.js file:///C:/Review - Tradiecore/HTML5 - Top Navigation.4.log.zip

--- Data/Search.js
+++ Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0,70],m:[],u:['../Content/A_Introduction Topics/Basic Steps.htm'],s:['$20','mail'],p:[['$20',15],['apps',80],['connecting',34],['enter',4],['image',52],['made',21],['or',41],['right',15],['tax',15],['to',118],['you',15]]});
+define({r:0.5,st:'Stem',n:1,t:[0,70],m:[],u:['../Content/A_Introduction Topics/Basic Steps.htm'],s:['$20','magnifi'],p:[['$20',15],['apps',79],['connected',118],['ensure',135],['image',26],['ludus',126],['or',35],['reviews',55],['tapping',108],['to',116],['you',15]]});
--- Resources/Scripts/require.config.js
+++ Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638636498922517786'
+    urlArgs: 't=638645236096291736'
 });
